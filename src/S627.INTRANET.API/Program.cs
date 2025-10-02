1ebd00f65bf6510f99132617977287bcef5fd6b6
Ismael Costa <ismaelbcbnb@gmail.com>
2025-10-02 03:31:04 -0300

    Program.cs

--- src/S627.INTRANET.API/Program.cs
+++ src/S627.INTRANET.API/Program.cs
@@ -1,27 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
-    {
-        Title = "S627 INTRANET API",
-        Version = "v1",
-        Description = "API para gerenciamento de parâmetros do sistema."
-    });
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "S627 INTRANET API v1");
-        c.RoutePrefix = string.Empty;
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
